3e25879438d41a015fee80766c3e82852e37a96e
QuyetTC <tranquyet02588x1112002@gmail.com>
2024-01-07 17:13:18 +0700

    Update Icon#

--- FileDirectory/MainWindow.xaml.cs
+++ FileDirectory/MainWindow.xaml.cs
@@ -33,33 +33,55 @@ namespace FileDirectory
 
         private void btn_name_Click(object sender, RoutedEventArgs e)
         {
-            Winforms.FolderBrowserDialog dialog = new Winforms.FolderBrowserDialog();
-            Winforms.DialogResult dialogResult = dialog.ShowDialog();
+            
+            tb_filedirectory.Text = "";
+           
             DataList = new ObservableCollection<MyData>
             {
 
 
             };
+            DataList.Clear();
+      
+ 
+
+            Winforms.FolderBrowserDialog dialog = new Winforms.FolderBrowserDialog();
+            Winforms.DialogResult dialogResult = dialog.ShowDialog();
 
             if (dialogResult == Winforms.DialogResult.OK)
             {
+            
                 string path = dialog.SelectedPath;
                 tb_filedirectory.Text = path;
-                DataList.Clear();
+         
                 try
                 {
                     string[] files = Directory.GetFiles(path);
                     string[] folder = Directory.GetDirectories(path);
                     
-                    lv_listfile.Items.Clear();
-                    
+                
+                    foreach (string file in folder)
+                    {
+                        string[] result = file.Split('\\');
+                        DataList.Add(new MyData
+                        {
+                            Name = $"{result[result.Length - 1]}",
+                            Path = $"{file}",
+                            Icon = "C:\\Users\\Admin\\Desktop\\PRN221\\Fileee\\vector-folder-icon.jpg"
+                        });
+                    };
+                  
                     foreach (string file in files)
                     {
                         string[] result = file.Split('\\');
-                        DataList.Add(new MyData { FolderName = $"{result[result.Length - 2]}", FileName = $"{result[result.Length - 1]}", FolderIcon = "" });
+                        DataList.Add(new MyData {
+                            Name = $"{result[result.Length - 1]}", 
+                            Path = $"{file}",
+                            Icon = "C:\\Users\\Admin\\Desktop\\PRN221\\Fileee\\file-icon.png"
+                        });
                     };
                     lv_listfile.ItemsSource = DataList;
-                   
+    
                 }
                 catch (Exception ex)
                 {
@@ -80,9 +102,9 @@ namespace FileDirectory
 
         public class MyData
         {
-            public string FolderName { get; set; }
-            public string FileName { get; set; }
-            public string FolderIcon { get; set; }
+            public string Path { get; set; }
+            public string Name { get; set; }
+            public string Icon { get; set; }
         }
     }
 }
